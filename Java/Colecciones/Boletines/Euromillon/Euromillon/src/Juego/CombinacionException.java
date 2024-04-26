8541dfd9db800409a570b5b640aff11ac65f80db
Joaquín García Téllez <102540516+teljoa@users.noreply.github.com>
2024-04-26 17:13:23 +0200

    Add files via upload

---
+++ Java/Colecciones/Boletines/Euromillon/Euromillon/src/Juego/CombinacionException.java
@@ -0,0 +1,15 @@
+package Juego;
+
+public class CombinacionException extends Exception {
+
+	public CombinacionException() {
+	}
+
+	public CombinacionException(String message) {
+		super(message);
+	}
+
+	public CombinacionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ Java/Colecciones/Boletines/Euromillon/Euromillon/src/Juego/HistorialException.java
@@ -0,0 +1,15 @@
+package Juego;
+
+public class HistorialException extends Exception {
+
+	public HistorialException() {
+	}
+
+	public HistorialException(String message) {
+		super(message);
+	}
+
+	public HistorialException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
