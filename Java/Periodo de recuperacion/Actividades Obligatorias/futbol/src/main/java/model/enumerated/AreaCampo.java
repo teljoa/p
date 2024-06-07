2a10b554b97f5f90f4f05c8d55dd5b7a5d1a1686
Joaquín García Téllez <102540516+teljoa@users.noreply.github.com>
2024-06-07 20:58:13 +0200

    Delete Java/Periodo de recuperacion/Actividades Obligatorias/futbol directory

--- Java/Periodo de recuperacion/Actividades Obligatorias/futbol/src/main/java/model/enumerated/AreaCampo.java
+++
@@ -1,5 +0,0 @@
-package main.java.model.enumerated;
-
-public enum AreaCampo {
-	PORTERIA,CENTRO,DEFENSA,CONTRARIO;
-}
--- Java/Periodo de recuperacion/Actividades Obligatorias/futbol/src/main/java/model/interf/Parable.java
+++
@@ -1,6 +0,0 @@
-package main.java.model.interf;
-
-public interface Parable {
-	
-	public double pararBalon();
-}
