1174c9d95805dea6c0f51fff76a03a9943e942f4
Joaquín García Téllez <102540516+teljoa@users.noreply.github.com>
2024-06-07 20:57:51 +0200

    Add files via upload

---
+++ Java/Periodo de recuperacion/Actividades Obligatorias/Hogwarts/src/main/java/model/Defensa.java
@@ -0,0 +1,10 @@
+package main.java.model;
+
+import java.time.LocalDate;
+
+public class Defensa extends Hechizo {
+	
+	public Defensa(String nombre, int puntos, LocalDate fechaLanzamiento) {
+		super(nombre, puntos, fechaLanzamiento);
+	}
+}
